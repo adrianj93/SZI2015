5dade7355ab389c515c78c6c944fe13a1993d6ed
adrianj93 <adrianj93@gmail.com>
2015-05-03 13:38:58 +0200

    WPF Grids

---
+++ Traktor/ApplicationWindowBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Traktor
+{
+	public class ApplicationWindowBase : Window
+	{
+		public void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				DragMove();
+		}
+
+		public void Button_WinClose_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		public void Button_WinMin_Click(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Minimized;
+		}
+	}
+
+}
