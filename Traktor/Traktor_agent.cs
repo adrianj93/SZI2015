efb5cf6bc9530cfecdf3cc3e7a3ccc9a9ba830f4
unknown <s383959@pc17e.labs.wmi.amu.edu.pl>
2015-05-04 18:37:01 +0200

    2 punkty

--- Traktor/Traktor_agent.cs
+++ Traktor/Traktor_agent.cs
@@ -10,16 +10,49 @@ namespace Traktor
 	{
 		private int paliwo_poziom;
 		private int paliwo_max;
+		private int poz_x;
+		private int poz_y;
+
+		private bool available_left = false;
+		private bool available_right = false;
+		private bool available_forward = false;
+
+		private int passable;
 
 		public Traktor_agent(int paliwo_poziom, int paliwo_max)
 		{
 			this.paliwo_poziom = paliwo_poziom;
 			this.paliwo_max = paliwo_max;
+
+			this.poz_x = 0;
+			this.poz_y = 0;
 		}
 
 		public int getPaliwo()
 		{
 			return paliwo_poziom;
+		}
+
+		public int getPosition_x()
+		{
+			return poz_x;
+		}
+
+		public int getPosition_y()
+		{
+			return poz_y;
+		}
+
+		public void setPosition(int x, int y)
+		{
+			this.poz_x = x;
+			this.poz_y = y;
+		}
+
+		public int detectNextBlock(int left, int right, int forward)
+		{
+			
+			return passable;
 		}
 
 		public void tankuj(int paliwo)
